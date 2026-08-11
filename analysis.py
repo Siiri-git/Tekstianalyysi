ec3dca419f52d669f2bc9ef8fc8ee52f44398b46
Siiri <sm7541@edu.turku.fi>
2026-08-11 11:37:20 +0300

    added demofile

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 # file = open("filename.txt", "r")
 # lines = file.readlines()
-txtFiles = {"article1", "article2", "article3", "article4"}
+txtFiles = {"article1", "article2", "article3", "article4", "demofile"}
 
 while True:
     print("Functions:\n1. Find a file\n2. Exit")
@@ -8,7 +8,15 @@ while True:
 
     if usrFunc == "1":
         usrFile = input("File name: ")
+        if usrFile in txtFiles:
+            print("yay")
+            file = open(usrFile, "r")
 
+        else:
+            print("try again")
 
     elif usrFunc == "2":
         break
+
+    else:
+        print("Try again!")
