9eb6cf5b1373fd82912ec0bae87b33e054159c12
Siiri <sm7541@edu.turku.fi>
2026-08-11 11:27:49 +0300

    created analysis file

---
+++ analysis.py
@@ -0,0 +1,13 @@
+# file = open("filename.txt", "r")
+# lines = file.readlines()
+
+while True:
+    print("Functions:\n1. Find a file\n2. Exit")
+    usrFunc = input("Choose a function: ")
+
+    if usrFunc == "1":
+        usrFile = input("File name: ")
+        
+
+    elif usrFunc == "2":
+        break
